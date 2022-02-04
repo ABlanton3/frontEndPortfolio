88fa30d8c89a81fcce33718450d7c842bf1f55b6
Amanda Blanton <77287184+ABlanton13@users.noreply.github.com>
2022-02-04 14:18:26 -0600

    Initialize some pages and components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import express from 'express';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const app = express()
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-app.listen(4242, () => {
-  console.log("The server is running.");
-})
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
